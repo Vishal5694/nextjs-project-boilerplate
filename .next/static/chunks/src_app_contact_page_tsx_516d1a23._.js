172e5faf6a0a241a5e2d2b27fce581c3160b0fa8
Vishal5694 <dhimanvishal5694@gmail.com>
2025-07-18 02:11:26 +0530

    Add .next/static/chunks/src_app_contact_page_tsx_516d1a23._.js 1752784886133

---
+++ .next/static/chunks/src_app_contact_page_tsx_516d1a23._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a2321dbf._.js",
+  "static/chunks/node_modules_56dc12f1._.js"
+],
+    source: "dynamic"
+});
